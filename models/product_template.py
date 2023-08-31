c4257d863ea3e2b618ea5f247e5d8cd1d65c814e
pplinarez <p.plinarez.c@gmail.com>
2023-08-31 10:28:00 -0500

    last updates

--- models/product_template.py
+++ models/product_template.py
@@ -1,7 +1,6 @@
 
 from odoo import models, fields, api
 
-
 class TKOpticalProduct(models.Model):
     _inherit = "product.template"
 
@@ -39,3 +38,38 @@ class TKOpticalProduct(models.Model):
 
     product_brand_id = fields.Many2one(
         "product.brand", string="Brand")    
+
+"""
+    def product_attributes(self, is_optical):
+        attributes = ["Brand", "Rim", "Type of glasses", "style", "Gender"]
+        product_attribute_lines =[]
+        existing_attributes = []
+        product_attribute_values=[]
+        if is_optical == True:
+            if self.attribute_lines_ids:
+                pass
+            else:  
+                for line in self.attribute_lines_ids:
+                    product_attribute_lines.append(line.display_name)
+                    product_attribute_values.append(line.value_ids.name)
+
+                    for attrs in product_attribute_lines:
+                        if attrs not in attributes:
+                            
+                            existing_attributes.append(attrs)
+                    
+                    
+
+
+      """"""          
+        #product_attribute_lines = self.env["product.template.attribute_line_ids"].search()
+        
+        if self.attribute_line_ids: 
+            pass
+        else: 
+            for attribute in attributes:
+                values = {}
+                values["active"]= True
+                values["attribute_id"] = search
+                create({"name": name })
+"""
